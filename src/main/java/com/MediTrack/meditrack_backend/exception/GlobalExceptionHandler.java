c86c1ce35d48ab45ad477ca50a1b8b3757ce2ffd
Kareem Meshal <kareemkhdh543@gmail.com>
2026-06-16 21:04:36 +0300

    -added exception logs for debugging

--- src/main/java/com/MediTrack/meditrack_backend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/MediTrack/meditrack_backend/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.MediTrack.meditrack_backend.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
@@ -11,8 +12,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -59,8 +62,15 @@ public class GlobalExceptionHandler {
     // ── Catch-all last ──────────────────────────────────────────────
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Map<String, Object>> handleGeneric(Exception ex) {
-        return buildResponse(HttpStatus.INTERNAL_SERVER_ERROR,
-                "An unexpected error occurred");
+
+        log.error("Unexpected error", ex); // if you use logger
+
+        return ResponseEntity.status(500).body(
+                Map.of(
+                        "status", 500,
+                        "error", "Internal Server Error"
+                )
+        );
     }
 
     private boolean isNotFoundError(String message) {
